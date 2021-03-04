c9ae721686684b1f8853aab671a3d58c35816dcc
Jncl <jncl@mac.com>
2021-03-04 18:45:11 +0000

    Add show_pref_changes.sh

---
+++ show_pref_changes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ls -t ~/Library/Preferences | head -n 5
+ls -t ~/Library/Preferences/ByHost | head -n 5
+ls -t /Library/Preferences | head -n 5
+ls -t /Library/Preferences/SystemConfiguration | head -n 5
