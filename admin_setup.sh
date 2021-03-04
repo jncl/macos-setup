6494000669f30b9a016d6efbdedb79124ac8a4cd
Jncl <jncl@mac.com>
2021-03-04 11:21:46 +0000

    Update admin_setup.sh

--- admin_setup.sh
+++ admin_setup.sh
@@ -9,4 +9,4 @@ sudo -H pip3 install -U ansible
 
 ansible-galaxy install -r requirements.yml
 
-caffeinate ansible-playbook admin_playbook.yml -i inventory -l localhost
+caffeinate ansible-playbook admin_playbook.yml -i inventory -l localhost -K mac_admin_pw
