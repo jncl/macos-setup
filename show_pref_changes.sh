8d012c54d87f2fd2ce47f31ad4911b78f27dcab2
Jncl <jncl@mac.com>
2021-03-04 18:48:07 +0000

    Update show_pref_changes.sh

--- show_pref_changes.sh
+++ show_pref_changes.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo ==> ~/Library/Preferences
+echo "==> ~/Library/Preferences file changes"
 ls -t ~/Library/Preferences | head -n 5
-echo ==> ~/Library/Preferences/ByHost
+echo "==> ~/Library/Preferences/ByHost file changes"
 ls -t ~/Library/Preferences/ByHost | head -n 5
-echo ==> /Library/Preferences
+echo "==> /Library/Preferences file changes"
 ls -t /Library/Preferences | head -n 5
-echo ==> /Library/Preferences/SystemConfiguration
+echo "==> /Library/Preferences/SystemConfiguration file changes"
 ls -t /Library/Preferences/SystemConfiguration | head -n 5
