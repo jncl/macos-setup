96d575b88db593899a1a39d69ae14857c207c516
Jncl <jncl@mac.com>
2021-03-04 11:54:19 +0000

    Update admin_setup.sh

--- admin_setup.sh
+++ admin_setup.sh
@@ -4,9 +4,17 @@
 # git clone https://github.com/jncl/macos-setup
 # cd macos-setup
 #
+
+# Upgrade to latest pip, required to prevent cryptography from failing to build when installing ansible
 sudo -H pip3 install -U pip
+# Install ansible
 sudo -H pip3 install -U ansible
 
+# Reset Sudo Password Timeout to prevent playbook from hanging
+sudo -k
+
+# Install playbook pre-requisites
 ansible-galaxy install -r requirements.yml
 
-caffeinate ansible-playbook admin_playbook.yml -i inventory -l localhost
+# Run playbook without sleeping
+caffeinate ansible-playbook admin_playbook.yml -i inventory -l localhost -K
