68eb228993491e3b23f10bb220525c3199c53b55
Jon Hogg <hogg.jon+github@gmail.com>
2023-06-11 16:34:28 +0100

    Update user_setup.sh

--- user_setup.sh
+++ user_setup.sh
@@ -6,6 +6,9 @@ git pull
 # Install ansible for user
 ./install_ansible.sh
 
+# Apply path updates (homebrew & ansible)
+eval $(/usr/libexec/path_helper)
+
 # Set Sudo timeout
 echo "Defaults timestamp_timeout=90" | sudo tee /etc/sudoers.d/00-user
 sleep 2
