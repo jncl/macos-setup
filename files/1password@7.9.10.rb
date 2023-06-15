311487186a896c05cacad172cecb1d03409e62b6
Jon Hogg <hogg.jon+github@gmail.com>
2023-06-15 22:37:14 +0100

    Updated files

--- files/1password@7.9.10.rb
+++ files/1password@7.9.10.rb
@@ -1,6 +1,6 @@
 cask "1password@7.9.10" do
   version "7.9.10"
-  sha256 "3aab2c425432a07faa629ab08ab0d82ed40af0cace13bbe83e8746c1cc3422b2"
+  sha256 "54b2151d14243f171d5a6d4cc0f1119374a92ac1b0790a692688c66edf72f696"
 
   url "https://cachefly-origin.agilebits.com/dist/1P/mac7/1Password-7.9.10.zip"
   name "1Password"
